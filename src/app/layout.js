2d10e120db596742bdcdd6aea79ceb68bb9182db
Bibash45 <bibashcdry46@gmail.com>
2025-02-05 15:48:15 +0545

    managing active link

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,18 @@
-import { Geist, Geist_Mono } from "next/font/google";
+"use client";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata = {
-  title: "NextJs Foundation",
-  description: "Learning the NextJs coreconcept",
-};
+import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         {children}
+        <ProgressBar
+          height="2px"
+          color="#db5235"
+          options={{ showSpinner: false }}
+          shallowRouting
+        />
       </body>
     </html>
   );
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,14 +1,19 @@
+"use client"
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const Menu = () => {
+  const pathname = usePathname()
+  console.log(pathname);
+  
   return (
     <div className='flex gap-4 justify-center'>
-      <Link href={`/`}>Home</Link>
-      <Link href={`/admin`}>Admin</Link>
-      <Link href={`/contact`}>Contact</Link>
-      <Link href={`/about`}>About</Link>
-      <Link href={`/brand/apple/iphone`}>Brand</Link>
+      <Link className={`${pathname === '/' ? 'active-link' : 'pending-link'}`} href={`/`}>Home</Link>
+      <Link className={`${pathname === '/admin' ? 'active-link' : 'pending-link'}`} href={`/admin`}>Admin</Link>
+      <Link className={`${pathname === '/contact' ? 'active-link' : 'pending-link'}`} href={`/contact`}>Contact</Link>
+      <Link className={`${pathname === '/about' ? 'active-link' : 'pending-link'}`} href={`/about`}>About</Link>
+      <Link className={`${pathname === '/brand/apple/iphone' ? 'active-link' : 'pending-link'}`} href={`/brand/apple/iphone`}>Brand</Link>
     </div>
   )
 }
