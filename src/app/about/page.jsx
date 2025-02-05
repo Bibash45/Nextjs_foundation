3e3fbf776eb80e21abde79e7700366e3b55d01e8
Bibash45 <bibashcdry46@gmail.com>
2025-02-05 16:05:22 +0545

    query with serachParams as props and useSearchParams from "use client"

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,28 @@
+"use client"
 import React from "react";
 import styles from "./aboutstyle.module.css";
 import Menu from "@/components/Menu";
-const page = () => {
-  return <div className={styles.myText}>
-    <Menu />
-    THis is an about page</div>;
+import { useSearchParams } from "next/navigation";
+
+const page = ({ searchParams }) => {
+  // const page = ({searchParams}) => {
+
+   const params = useSearchParams();
+
+  return (
+    <div className={styles.myText}>
+      <Menu />
+
+      <h1>THis is an about page</h1>
+
+      {/* <h2>{searchParams.name}</h2>
+      <h2>{searchParams.price}</h2> */}
+
+
+      <h2>{params.get('name')}</h2>
+      <h2>{params.get("price")}</h2>
+    </div>
+  );
 };
 
 export default page;
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,14 +5,16 @@ import React from 'react'
 
 const Menu = () => {
   const pathname = usePathname()
-  console.log(pathname);
   
   return (
     <div className='flex gap-4 justify-center'>
       <Link className={`${pathname === '/' ? 'active-link' : 'pending-link'}`} href={`/`}>Home</Link>
       <Link className={`${pathname === '/admin' ? 'active-link' : 'pending-link'}`} href={`/admin`}>Admin</Link>
       <Link className={`${pathname === '/contact' ? 'active-link' : 'pending-link'}`} href={`/contact`}>Contact</Link>
-      <Link className={`${pathname === '/about' ? 'active-link' : 'pending-link'}`} href={`/about`}>About</Link>
+      <Link className={`${pathname === '/about' ? 'active-link' : 'pending-link'}`} href={{
+        pathname: '/about',
+        query: { name: 'laptop',price:'200' }
+      }}>About</Link>
       <Link className={`${pathname === '/brand/apple/iphone' ? 'active-link' : 'pending-link'}`} href={`/brand/apple/iphone`}>Brand</Link>
     </div>
   )
