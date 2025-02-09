bce65ffd4a52a32e502fabe125422971d352092c
Bibash45 <bibashcdry46@gmail.com>
2025-02-09 13:24:50 +0545

    Image Optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,42 +1,45 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["glowglide.shop"],
+  },
   // distDir:'build',
-  compress:false,
-  httpAgentOptions: {
-    keepAlive: true,
-    timeout: 60000,
-  },
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          //   {
-          //     key: "Content-Security-Policy",
-          //     value:
-          //       "default-src 'self'; script-src 'self'; style-src 'www.google.com'",
-          //   },
-          //   {
-          //     key: "Permissions-Policy",
-          //     value:
-          //       "camera=();battery=(self);browsing-topics=();geolocation=();microphone=('https://domain.com')",
-          //   },
-          //   {
-          //     key: "Stick-transport-security",
-          //     value:
-          //       "max-age=63072000;includeSubDomains;preload",
-          //   },
-          //   {
-          //     key: "X-Content-Type-Options",
-          //     value: "nosniff",
-          {
-            key: "X-Powered-BY",
-            value: "false",
-          },
-        ],
-      },
-    ];
-  },
+  //   compress:false,
+  //   httpAgentOptions: {
+  //     keepAlive: true,
+  //     timeout: 60000,
+  //   },
+  //   async headers() {
+  //     return [
+  //       {
+  //         source: "/:path*",
+  //         headers: [
+  //           //   {
+  //           //     key: "Content-Security-Policy",
+  //           //     value:
+  //           //       "default-src 'self'; script-src 'self'; style-src 'www.google.com'",
+  //           //   },
+  //           //   {
+  //           //     key: "Permissions-Policy",
+  //           //     value:
+  //           //       "camera=();battery=(self);browsing-topics=();geolocation=();microphone=('https://domain.com')",
+  //           //   },
+  //           //   {
+  //           //     key: "Stick-transport-security",
+  //           //     value:
+  //           //       "max-age=63072000;includeSubDomains;preload",
+  //           //   },
+  //           //   {
+  //           //     key: "X-Content-Type-Options",
+  //           //     value: "nosniff",
+  //           {
+  //             key: "X-Powered-BY",
+  //             value: "false",
+  //           },
+  //         ],
+  //       },
+  //     ];
+  //   },
 };
 
 export default nextConfig;
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
 import React from "react";
 
 const page = () => {
-  return <div className="global-text">Profile page ho yo ke xa</div>;
+  return <div className="global-text">
+    <Image src={`https://glowglide.shop/cdn/shop/files/Untitled_design__34_-removebg-preview_1.png?v=1703970223&width=535`} alt='beauty' height={300} width={300}
+    layout="fixed"
+    priority
+    />
+  </div>;
 };
 
 export default page;
