d5a205d8f3dad1839d38574a846a1480779ff38c
Bibash45 <bibashcdry46@gmail.com>
2025-02-09 12:01:31 +0545

    headers concept

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/about",
-        headers: [{ key: "app-auth-key", value: "3253GSJO" }],
-      },
-    ];
-  },
+    // distDir:'build',
+    async headers() {
+      return [
+        {
+          source: "/:path*",
+          headers: [
+            //   {
+            //     key: "Content-Security-Policy",
+            //     value:
+            //       "default-src 'self'; script-src 'self'; style-src 'www.google.com'",
+            //   },
+            //   {
+            //     key: "Permissions-Policy",
+            //     value:
+            //       "camera=();battery=(self);browsing-topics=();geolocation=();microphone=('https://domain.com')",
+            //   },
+            //   {
+            //     key: "Stick-transport-security",
+            //     value:
+            //       "max-age=63072000;includeSubDomains;preload",
+            //   },
+            //   {
+            //     key: "X-Content-Type-Options",
+            //     value: "nosniff",
+              {
+                key: "X-Powered-BY",
+                value: "false",
+              },
+          ],
+        },
+      ];
+    },
+
+
 };
 
 export default nextConfig;
